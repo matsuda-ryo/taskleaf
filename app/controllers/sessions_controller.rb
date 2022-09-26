3eed362eb8e70d1bd38161208486f416fe2c9f32
matsuda-ryo <kingocean3569@gmail.com>
2022-09-26 21:27:06 +0900

    ログインしているユーザーのTaskデータの登録、ログインしているユーザーのTaskデ ータだけを読みだす、管理機能を管理者ユーザーだけに利用させるようにする。、最初の管理者ユーザーを作る、タスク一覧を作成日時の新しい順に表示する。、scopeを活用する。、フィルタを使い重複を避ける

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     
     if user&.authenticate(session_params[:password])
-      session[:user_id] = user.index
+      session[:user_id] = user.id
       redirect_to  root_url, rotice: 'ログインしました。'
     else
       render :new
